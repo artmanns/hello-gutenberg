b1e1ab8e32ee612d054c38787c07f937a682cced
Karl Artmann <charly@artmanns.at>
2022-03-29 10:03:08 +0200

    added .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  globals: {
+    wp: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "no-console": 1,
+  },
+};
